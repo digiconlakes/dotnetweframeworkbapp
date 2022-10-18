e7ef7031f71f08d69c489d5de9132884735b043d
Kanaiya <katarmal.kanaiya@gmail.com>
2022-10-18 15:15:58 +0530

    code update

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +9,9 @@ namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        public string username = "Nice to meet you!";
+        public string password = "Nice to meet you!";
+
         public ActionResult Index()
         {
             return View();
@@ -17,6 +21,12 @@ namespace WebApplication.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
+
+            if (true);
+            {
+
+            }
+
             return View();
         }
 
@@ -25,6 +35,16 @@ namespace WebApplication.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public void fakePage()
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb;User ID=sa;Password=demol23";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            cnn.Close();
         }
     }
 }
